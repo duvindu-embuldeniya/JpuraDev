a20479840545835bcb72b5b5fcd56b5bcd887fef
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-06-19 01:02:02 +0530

    adjust forms

--- simply/home/forms.py
+++ simply/home/forms.py
@@ -6,3 +6,11 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'image', 'description', 'source_link']
+    
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        # self.fields['title'].widget.attrs.update({'placeholder':'title'})
+
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
